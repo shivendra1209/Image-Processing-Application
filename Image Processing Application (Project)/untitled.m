442eb74b4b74b9c63e3ca0c574085a8783e48e84
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2019-04-04 18:01:28 +0530

    Add files via upload
    An Image Processing Application made using MATLAB. This application can be used to edit images and perform multiple operations on images. This application is completely made using Image Processing tools and MATLAB.

---
+++ Image Processing Application (Project)/untitled.m
@@ -0,0 +1,303 @@
+
+function varargout = untitled(varargin)
+% UNTITLED MATLAB code for untitled.fig
+%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%
+%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled
+
+% Last Modified by GUIDE v2.5 29-May-2018 08:50:56
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled is made visible.
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% Choose default command line output for untitled
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global abc;
+[filename,pathname] = uigetfile({'*.jpg';'*.jpeg';},'File Selector');
+image = strcat(pathname,filename);
+axes(handles.axes1);
+imshow(image);
+abc = imread(image);
+handles.abc=abc;
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global abc_gray;
+abc=handles.abc;
+abc_gray=rgb2gray(abc);
+axes(handles.axes1);
+imshow(abc_gray);
+handles.abc_gray=abc_gray;
+guidata(hObject,handles);
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+abc_gray=handles.abc_gray;
+brightness=get(hObject,'Value');
+output=imadd(abc_gray,brightness);
+axes(handles.axes2);
+imshow(output);
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+abc_gray=handles.abc_gray;
+
+axes(handles.axes2);
+imshow(histogram(abc_gray));
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+axes(handles.axes2);
+imshow(histeq(abc_gray));
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+abc_negative=255-abc_gray;
+axes(handles.axes2);
+imshow(abc_negative);
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+abc_double = im2double(abc_gray);
+prompt = {'Enter constant to be multiplied with log function:'};
+dlg_title = 'Constant';
+answer = inputdlg(prompt,dlg_title);
+answer = str2num(answer{1});
+abc_log =(answer)*log(abc_double+1);
+axes(handles.axes2);
+imshow(abc_log);
+guidata(hObject, handles);
+
+
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clc;
+clearall = 'clear all';
+evalin('base',clearall);
+delete(handles.figure1);
+guidata(hObject, handles);
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+axes(handles.axes1);
+cla();
+axes(handles.axes2);
+cla();
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton9.
+function pushbutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+abc_double = im2double(abc_gray);
+prompt = {'Enter Constant to be multiplied with power funnction:','Enter Gamma Value:'};
+dlg_title = 'User Input';
+num_lines = 1;
+answer = inputdlg(prompt,dlg_title,num_lines);
+constant = str2num(answer{1});
+gamma = str2num(answer{2});
+abc_power = constant*(abc_double).^(gamma);
+abc_max = max(max(abc_power));
+abc_norm = abc_power/abc_max; 
+abc_norm=255*abc_norm;
+axes(handles.axes2);
+imshow(uint8(abc_norm));
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton10.
+function pushbutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+mask=[-1 0 1;-1 0 1;-1 0 1];
+abc_prewitt_vertical = conv2(mask,abc_gray);
+mask2 = [-1 -1 -1;0 0 0;1 1 1];
+abc_prewitt_horizontal = conv2(mask2,abc_prewitt_vertical);
+axes(handles.axes2);
+imshow(abc_prewitt_horizontal,[0 255]);
+guidata(hObject, handles);
+
+
+% --- Executes on button press in radiobutton1.
+function radiobutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton1
+abc_gray=handles.abc_gray;
+mask = [0 1 0;1 -4 1;0 1 0];
+abc_positive = conv2(mask,abc_gray);
+axes(handles.axes2);
+imshow(abc_positive,[0 255]);
+guidata(hObject, handles);
+
+
+% --- Executes on button press in radiobutton2.
+function radiobutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton2
+abc_gray=handles.abc_gray;
+mask = [0 -1 0;-1 4 -1;0 -1 0];
+abc_negative = conv2(mask,abc_gray);
+imshow(abc_negative,[0 255]);
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton12.
+function pushbutton12_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+abc_double = im2double(abc_gray);
+abc_fft=fft2(abc_double);
+axes(handles.axes2);
+imagesc(abs(fftshift(abc_fft)));
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton13.
+function pushbutton13_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+abc_gray=handles.abc_gray;
+prompt = {'Enter standard deviation value:'};
+dlg_title = 'Constant';
+answer = inputdlg(prompt,dlg_title);
+answer = str2num(answer{1});
+abc_blur = imgaussfilt(abc_gray,answer);
+axes(handles.axes2);
+imshow(abc_blur);
+guidata(hObject, handles);guide
